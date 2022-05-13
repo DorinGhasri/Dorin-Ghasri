387e11e49687873f8959d9cec6b50fd89b70aba5
Dorin Ghasri <ghasr@post.bgu.ac.il>
2022-05-14 00:33:27 +0300

    update JS

--- static/DogsPageJS.js
+++ static/DogsPageJS.js
@@ -28,13 +28,8 @@ function myTyping(){
 }
 
 
-
-
 //pull the pathname from window location
 const activePage = window.location.pathname;
-console.log(window);
-console.log(window.location);
-console.log(activePage);
 
 /*create an arey of the links in nav, 
 compare each to pathname and mark the one that is active
@@ -45,39 +40,4 @@ const navLinks = document.querySelectorAll('nav a').forEach(link => {
   }
 });
      
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
-    center: new google.maps.LatLng(31.2573952, 34.8028928),
-    mapTypeId: "terrain",
-  });
-
-  // Create a <script> tag and set the USGS URL as the source.
-  const script = document.createElement("script");
-
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src =
-    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
-  document.getElementsByTagName("head")[0].appendChild(script);
-}
-
-
-// Loop through the results array and place a marker for each
-// set of coordinates.
-const eqfeed_callback = function (results) {
-  for (let i = 0; i < results.features.length; i++) {
-    const coords = results.features[i].geometry.coordinates;
-    const latLng = new google.maps.LatLng(coords[1], coords[0]);
-
-    new google.maps.Marker({
-      position: latLng,
-      map: map,
-    });
-  }
-};
-
-window.initMap = initMap;
-window.eqfeed_callback = eqfeed_callback;
-
 
